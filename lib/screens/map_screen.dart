959de41bce8c545858231aded513ea9a5274d239
kristiyana milcheva <kristiyana.milcheva@gmail.com>
2023-03-02 20:22:07 +0100

    marker

--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -17,10 +17,30 @@ class MapScreen extends StatefulWidget {
 }
 
 class _MapScreenState extends State<MapScreen> {
+  LatLng? _pickedLocation;
+
+  void _selectLocation(LatLng position) {
+    setState(() {
+      _pickedLocation = position;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Your Map')),
+      appBar: AppBar(
+        title: const Text('Your Map'),
+        actions: [
+          if (widget.isSelecting)
+            IconButton(
+                onPressed: _pickedLocation == null
+                    ? null
+                    : () {
+                        Navigator.of(context).pop(_pickedLocation);
+                      },
+                icon: const Icon(Icons.check))
+        ],
+      ),
       body: GoogleMap(
         initialCameraPosition: CameraPosition(
           target: LatLng(
@@ -29,6 +49,13 @@ class _MapScreenState extends State<MapScreen> {
           ),
           zoom: 16,
         ),
+        onTap: widget.isSelecting ? _selectLocation : null,
+        markers: _pickedLocation == null
+            ? {}
+            : {
+                Marker(
+                    markerId: const MarkerId('m1'), position: _pickedLocation!),
+              },
       ),
     );
   }
--- lib/widgets/location_input.dart
+++ lib/widgets/location_input.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:great_places/helpers/location_helper.dart';
 import 'package:great_places/screens/map_screen.dart';
 import 'package:location/location.dart';
@@ -23,7 +24,7 @@ class _LocationInputState extends State<LocationInput> {
   }
 
   Future<void> _selectOnMap() async {
-    final selectedLocation = await Navigator.of(context).push(
+    final LatLng? selectedLocation = await Navigator.of(context).push(
       MaterialPageRoute(
         fullscreenDialog: true,
         builder: (context) => const MapScreen(
@@ -32,7 +33,7 @@ class _LocationInputState extends State<LocationInput> {
       ),
     );
     if (selectedLocation == null) return;
-
+    print(selectedLocation);
   }
 
   @override
