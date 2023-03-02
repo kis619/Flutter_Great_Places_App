017a4ac70131b628e5b9f0a9dbb8cf32f8173db2
kristiyana milcheva <kristiyana.milcheva@gmail.com>
2023-03-02 17:58:57 +0100

    added a dynamic map

--- README.md
+++ README.md
@@ -1,16 +1,9 @@
 # great_places
 
-A new Flutter project.
+Notes to self:
 
-## Getting Started
+so this should be added to - android/app/src/main/AndroidManifest.xml -
+<meta-data android:name="com.google.android.geo.API_KEY"
+        android:value="API"/>
 
-This project is a starting point for a Flutter application.
-
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
-
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+and have the .env file with the API
--- lib/widgets/location_input.dart
+++ lib/widgets/location_input.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:great_places/helpers/location_helper.dart';
+import 'package:great_places/screens/map_screen.dart';
 import 'package:location/location.dart';
 
 class LocationInput extends StatefulWidget {
@@ -19,6 +20,19 @@ class _LocationInputState extends State<LocationInput> {
     setState(() {
       _previewImageUrl = staticMapImageUrl;
     });
+  }
+
+  Future<void> _selectOnMap() async {
+    final selectedLocation = await Navigator.of(context).push(
+      MaterialPageRoute(
+        fullscreenDialog: true,
+        builder: (context) => const MapScreen(
+          isSelecting: true,
+        ),
+      ),
+    );
+    if (selectedLocation == null) return;
+
   }
 
   @override
@@ -54,7 +68,7 @@ class _LocationInputState extends State<LocationInput> {
                   foregroundColor: Theme.of(context).primaryColor),
             ),
             TextButton.icon(
-              onPressed: () {},
+              onPressed: _selectOnMap,
               icon: const Icon(Icons.map),
               label: const Text('Select on Map'),
               style: TextButton.styleFrom(
