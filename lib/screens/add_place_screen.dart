98de7a137ef1e8d9856312aa9d5880f261802c08
kristiyana milcheva <kristiyana.milcheva@gmail.com>
2023-03-02 15:54:38 +0100

    preapred location input for adding an additional location services

--- lib/screens/add_place_screen.dart
+++ lib/screens/add_place_screen.dart
@@ -3,9 +3,13 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:great_places/providers/great_places.dart';
 import 'package:provider/provider.dart';
+
+
+import '../providers/great_places.dart';
 import '../widgets/image_input.dart';
+import '../widgets/location_input.dart';
+
 
 class AddPlaceScreen extends StatefulWidget {
   static const routeName = '/add-place';
@@ -53,6 +57,8 @@ class _AddPlaceScreenState extends State<AddPlaceScreen> {
                     ),
                     const SizedBox(height: 10),
                     ImageInput(onSelectImage: _selectImage),
+                    const SizedBox(height: 10),
+                    LocationInput(),
                   ],
                 ),
               ),
---
+++ lib/widgets/location_input.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class LocationInput extends StatefulWidget {
+  const LocationInput({super.key});
+
+  @override
+  State<LocationInput> createState() => _LocationInputState();
+}
+
+class _LocationInputState extends State<LocationInput> {
+  String? _previewImageUrl;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          decoration: BoxDecoration(
+            border: Border.all(width: 1, color: Colors.grey),
+          ),
+          height: 170,
+          width: double.infinity,
+          alignment: Alignment.center,
+          child: _previewImageUrl == null
+              ? const Text(
+                  'No location chosen',
+                  textAlign: TextAlign.center,
+                )
+              : Image.network(
+                  _previewImageUrl!,
+                  fit: BoxFit.cover,
+                  width: double.infinity,
+                ),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextButton.icon(
+              onPressed: () {},
+              icon: const Icon(Icons.location_on),
+              label: const Text('Current location'),
+              style: TextButton.styleFrom(
+                  foregroundColor: Theme.of(context).primaryColor),
+            ),
+            TextButton.icon(
+              onPressed: () {},
+              icon: const Icon(Icons.map),
+              label: const Text('Select on Map'),
+              style: TextButton.styleFrom(
+                  foregroundColor: Theme.of(context).primaryColor),
+            ),
+          ],
+        )
+      ],
+    );
+  }
+}
